c789767454e717b02617d2b314f206fbd8923e8e
Domas <domas.jablonskis@gmail.com>
2020-11-07 10:21:55 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/domas/App.java
@@ -0,0 +1,12 @@
+package se.lexicon.domas;
+
+import java.util.Arrays;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        int[]numbers = {11,23,39};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
